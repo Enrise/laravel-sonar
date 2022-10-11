d34219a121562a7fc55430d3c0c350f542223892
Vincent Hagen <vinnie@script4web.nl>
2022-10-11 13:57:54 +0200

    Fixing service provider

--- src/Infrastructure/ServiceProviders/LaravelSonarServiceProvider.php
+++ src/Infrastructure/ServiceProviders/LaravelSonarServiceProvider.php
@@ -4,25 +4,31 @@ declare(strict_types=1);
 
 namespace Enrise\LaravelSonar\Infrastructure\ServiceProviders;
 
+use Enrise\LaravelSonar\Application\TransactionService;
+use Enrise\LaravelSonar\Application\TransactionServiceInterface;
+use Enrise\LaravelSonar\Domain\TransactionFailureRepositoryInterface;
+use Enrise\LaravelSonar\Domain\TransactionRepositoryInterface;
 use Enrise\LaravelSonar\Infrastructure\EventListeners\CommandEventSubscriber;
+use Enrise\LaravelSonar\Infrastructure\Repositories\TransactionFailureRepository;
+use Enrise\LaravelSonar\Infrastructure\Repositories\TransactionRepository;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 final class LaravelSonarServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap the application services.
      */
     public function boot(): void
     {
         Route::macro('sonar', static function (): void {
-            Route::group(base_path('routes/sonar.php'));
+            Route::group(__DIR__ . '/../../../routes/sonar.php');
         });
 
          $this->loadViewsFrom(__DIR__.'/../../resources/views', 'laravel-sonar');
          $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-         $this->loadRoutesFrom(__DIR__.'/../routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
@@ -43,6 +49,10 @@ final class LaravelSonarServiceProvider extends ServiceProvider
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../../../config/laravel-sonar.php', 'laravel-sonar');
+
+        $this->app->bind(TransactionServiceInterface::class, TransactionService::class);
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
+        $this->app->bind(TransactionFailureRepositoryInterface::class, TransactionFailureRepository::class);
 
         Event::subscribe(CommandEventSubscriber::class);
     }
