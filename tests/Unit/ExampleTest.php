52ed2e395144b8ce195e9f4a66d57ce941051790
Vincent Hagen <vinnie@script4web.nl>
2022-10-11 10:08:06 +0200

    Add taskfile, codestyling and pipeline

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Enrise\LaravelSonar\Tests\Unit;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testItSucceeds(): void
+    {
+        Assert::assertTrue(true);
+    }
+}
