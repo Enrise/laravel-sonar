05a2f416c12357d1df70e77749a94448b0943abb
Vincent Hagen <vinnie@script4web.nl>
2022-10-11 09:29:35 +0200

    Add bare sekeleton for a laravel package

---
+++ src/Infrastructure/ServiceProviders/LaravelSolarServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Enrise\LaravelSonar\Infrastructure\ServiceProviders;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSolarServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-solar');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-solar');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('laravel-solar.php'),
+            ], 'config');
+
+            // Publishing the views.
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-solar'),
+            ], 'views');*/
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/laravel-solar'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-solar'),
+            ], 'lang');*/
+
+            // Registering package commands.
+            // $this->commands([]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'laravel-solar');
+
+    }
+}
